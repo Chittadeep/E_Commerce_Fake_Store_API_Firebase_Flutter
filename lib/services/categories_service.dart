acff0c3d77026e4c5729c06445ef1118b8d1b6a1
Chittadeep Intern <chittadeep@ibarts.in>
2025-06-22 12:25:27 +0530

    feat: update fetchData method to return List<String> for improved type safety

--- lib/services/categories_service.dart
+++ lib/services/categories_service.dart
@@ -4,14 +4,15 @@ import 'dart:developer';
 import 'package:http/http.dart' as http;
 
 class CategoriesService {
-  Future<List?> fetchData() async {
+  Future<List<String>?> fetchData() async {
     try {
       final response = await http
           .get(Uri.parse('https://fakestoreapi.com/products/categories'));
 
       if (response.statusCode == 200) {
         List data = json.decode(response.body);
-        return data;
+        List<String> categories = List<String>.from(data);
+        return categories;
       } else {
         log("Error: ${response.statusCode}");
         return null;
