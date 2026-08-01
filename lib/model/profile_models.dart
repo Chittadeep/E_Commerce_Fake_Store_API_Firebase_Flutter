fffb471ba753f7c71fc7abd88a069f9cda0a85f2
chittadeep <mailchittadeep@gmail.com>
2026-08-02 01:39:58 +0530

    feat: expand OrderStatus enum and enhance OrderSummary model with additional fields and JSON serialization support

--- lib/model/profile_models.dart
+++ lib/model/profile_models.dart
@@ -1,7 +1,8 @@
 // lib/model/profile_models.dart
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-enum OrderStatus { delivered, inTransit }
+enum OrderStatus {all, delivered, inTransit, cancelled, processing}
 
 class SavedAddress {
   const SavedAddress({
@@ -19,12 +20,43 @@ class OrderSummary {
   const OrderSummary({
     required this.id,
     required this.status,
+    required this.title,
+    required this.photo,
     required this.date,
     required this.amount,
+    required this.quantity
   });
 
-  final String id;
+  final int id;
+  final String title;
+  final String photo;
   final OrderStatus status;
-  final String date;
+  final DateTime date;
   final double amount;
+  final int quantity;
+
+  factory OrderSummary.fromJson(Map<String, dynamic> json) {
+    return OrderSummary(
+      id: json['id'],
+      status: OrderStatus.values.firstWhere(
+            (value) => value.name == json['status'],
+        orElse: () => OrderStatus.inTransit,
+      ),
+      title: json['title'] as String,
+      photo: json['photo'] as String,
+      amount: double.parse(json['amount']),
+      quantity: json['quantity'],
+      date: (json['date'] as Timestamp).toDate(),
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'status': status.name,
+    'date': date,
+    'title': title,
+    'photo': photo,
+    'amount': amount.toString(),
+    'quantity': quantity,
+  };
 }
